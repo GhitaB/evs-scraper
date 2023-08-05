fb0ec75a0e199b79a3570f2ccd5bbd169108235a
GhitaB <ghitabizau27@gmail.com>
2023-08-05 23:11:58 +0300

    Get books categories.

--- src/evs_scraper/scraper.py
+++ src/evs_scraper/scraper.py
@@ -1,2 +1,33 @@
+SITE = "https://viatasisanatate.ro"
+BASE = "/produse/carti/"
+BOOKS_CATEGORIES = [
+    "sanatate",
+    "spiritualitate",
+    "imnuri",
+    "copii",
+    "familie",
+    "dezvoltare-personala",
+    "pachete-de-carte",
+    "cultura-generala",
+    "poezie",
+    "biografie",
+    "fictiune",
+]
+
+def human_readable_category(category):
+    return category.capitalize().replace("-", " ")
+
+def url_category(category):
+    return SITE + BASE + category
+
 def main():
-    print("WIP")
+    print("---------------------------------------------------------------------------")
+    print("Salut! Bun venit la", SITE)
+    print("Esti omul care vrea toata lista de carti intr-o singura pagina? Rezolvam...")
+    print("(Shh... Pe sub mana... Dar n-am incotro ca asa-s site-urile moderne.)")
+    print("---------------------------------------------------------------------------")
+
+    print("Categorii de carti: ")
+    for category in BOOKS_CATEGORIES:
+        print("-->", human_readable_category(category), " -- ", url_category(category))
+    print("---------------------------------------------------------------------------")
