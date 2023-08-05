bfcbb4dbf855066d8c32d59fb480953fb3e07228
GhitaB <ghitabizau27@gmail.com>
2023-08-05 22:39:19 +0300

    Add base structure

---
+++ src/evs_scraper/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='evs_scraper',
+   version='1.0',
+   description='EVS Scraper',
+   author='Ghita Bizau',
+   author_email='ghitabizau27@gmail.com',
+   packages=[],
+   install_requires=[],
+)
