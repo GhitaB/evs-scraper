7acf4adb8080552e0e20173196331571c0c0f13b
GhitaB <ghitabizau27@gmail.com>
2023-08-06 10:28:07 +0300

    Code style.

--- src/evs_scraper/scraper.py
+++ src/evs_scraper/scraper.py
@@ -21,36 +21,44 @@ BOOKS_CATEGORIES = [
 LIMIT_MAX_PRICE = None  # use None for no limit
 EXCLUDE_DUPLICATE = False  # True: include only books you don't have
 HARD_EXCLUDE = False  # True: use OR instead of AND in expression title-author
-USE_BLACKLIST_AS_WHITELIST = False  # True: if you want to see only the books you have
+USE_BLACKLIST_AS_WHITELIST = False  # True: see only the books you have
 SEARCH_PRINTRE_CARTI = True  # True: try to find the books on printrecarti.ro
+
 
 def human_readable_category(category):
     return category.capitalize().replace("-", " ")
 
+
 def url_category(category):
     return SITE + BASE + category
 
+
 def intro():
-    print("---------------------------------------------------------------------------")
+    print("------------------------------------------------------------------")
     print("Salut! Bun venit la", SITE)
-    print("Esti omul care vrea toata lista de carti intr-o singura pagina? Rezolvam...")
-    print("(Shh... Pe sub mana... Dar n-am incotro ca asa-s site-urile moderne.)")
+    print("Esti omul care vrea toata lista de carti intr-o singura pagina?")
+    print("(Rezolvam. Shh... Pe sub mana. Asa-s site-urile moderne...)")
+
 
 def list_categories():
-    print("---------------------------------------------------------------------------")
+    print("------------------------------------------------------------------")
     print("Categorii de carti: ")
     for category in BOOKS_CATEGORIES:
-        print("-->", human_readable_category(category), " -- ", url_category(category))
+        print("-->", human_readable_category(category),
+              " -- ", url_category(category))
+
 
 def get_page_html(url):
     try:
         with urlopen(url) as response:
             html_content = response.read().decode('utf-8')
     except Exception as e:
+        e = e
         # print(f"Error: {e}")
         html_content = None
 
     return html_content
+
 
 def get_category_details(category):
     url = url_category(category)
@@ -68,22 +76,27 @@ def get_category_details(category):
             with open("blacklist.txt", "r") as file:
                 BOOKS_I_HAVE = file.read()
     except FileNotFoundError:
-        print("ATENTIE: Nu ai creat fisierul blacklist.txt cu lista cartilor tale.")
+        print("ATENTIE: Nu ai creat blacklist.txt cu lista cartilor tale.")
 
     for book in books:
-        book_link = book.find("span", {"class": "hikashop_product_name"}).find("a")
+        book_link = book.find(
+            "span", {"class": "hikashop_product_name"}).find("a")
         book_url = book_link.attrs['href'].strip()
         h_url = SITE + book_url
         book_title = book_link.text.strip()
         try:
+            class_a = "hikashop_product_custom_autor_carte_line"
+            class_b = "hikashop_product_custom_value"
             author = book.find(
-                    "dl", {"class": "hikashop_product_custom_autor_carte_line"}).find(
-                            "dd", {"class": "hikashop_product_custom_value"}).text.strip()
+                "dl", {"class": class_a}).find(
+                "dd", {"class": class_b}).text.strip()
         except Exception:
             author = "???"
 
-        price = book.find("span", {"class": "hikashop_product_price_full"}).text
-        h_price = float(price.replace(" lei ", "").split("lei")[-1].replace(",", "."))
+        price = book.find(
+            "span", {"class": "hikashop_product_price_full"}).text
+        h_price = float(price.replace(" lei ", "").split(
+            "lei")[-1].replace(",", "."))
 
         this_book = {
             "url": h_url,
@@ -96,11 +109,13 @@ def get_category_details(category):
             # Not perfect. TODO: improve this
             is_duplicate = False
             if HARD_EXCLUDE is True:
-                if this_book['author'] in BOOKS_I_HAVE or this_book['title'] in BOOKS_I_HAVE:
+                if this_book['author'] in BOOKS_I_HAVE or \
+                        this_book['title'] in BOOKS_I_HAVE:
                     print("DUPLICATE: ", this_book)
                     is_duplicate = True
             else:
-                if this_book['author'] in BOOKS_I_HAVE and this_book['title'] in BOOKS_I_HAVE:
+                if this_book['author'] in BOOKS_I_HAVE and \
+                        this_book['title'] in BOOKS_I_HAVE:
                     print("DUPLICATE: ", this_book)
                     is_duplicate = True
 
@@ -122,8 +137,9 @@ def get_category_details(category):
         "books_list": books_list,
     }
 
+
 def get_all_books():
-    print("---------------------------------------------------------------------------")
+    print("------------------------------------------------------------------")
     all_books = []
     all_authors = []
     total_price = 0
@@ -146,13 +162,14 @@ def get_all_books():
             total_price += book['price']
             book_id += 1
 
-    print("---------------------------------------------------------------------------")
+    print("------------------------------------------------------------------")
     print("Iata si lista tuturor autorilor:")
     print(all_authors)
-    print("---------------------------------------------------------------------------")
+    print("------------------------------------------------------------------")
     print("PRET TOTAL, NUMAI ASTAZI, EVIDENT:", total_price, "lei")
-    print("---------------------------------------------------------------------------")
+    print("------------------------------------------------------------------")
     return all_books
+
 
 def search_printre_carti(all_books):
     url = "https://www.printrecarti.ro/?cauta="
@@ -160,7 +177,9 @@ def search_printre_carti(all_books):
     found_id = 1
 
     for book in all_books:
-        search_url = url + book['author'].replace(" ", "+") + "+" + book['title'].replace(" ", "+")
+        search_url = url + \
+            book['author'].replace(" ", "+") + "+" + \
+            book['title'].replace(" ", "+")
         html_doc = get_page_html(search_url)
         if html_doc is None:
             continue
@@ -171,9 +190,11 @@ def search_printre_carti(all_books):
         if not_found_flag is None:
             product_details = soup.find("div", {"class": "produs"})
             if "IN STOC" in product_details.text:
-                print(found_id, "PRINTRE CARTI: ", book['author'], book['title'], book['price'])
+                print(found_id, "PRINTRE CARTI: ",
+                      book['author'], book['title'], book['price'])
                 print(search_url)
                 found_id += 1
+
 
 def main():
     intro()
